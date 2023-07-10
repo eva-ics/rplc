f35cfc1d8d90120ba62f8be671ed623d1e9841f1
Serhij S <div@altertech.com>
2023-07-10 02:01:36 +0200

    0.2.3

--- src/io/modbus/types.rs
+++ src/io/modbus/types.rs
@@ -2,18 +2,18 @@ use eva_common::{EResult, Error, ErrorKind};
 use std::ops::{Deref, DerefMut};
 
 pub trait SwapModbusEndianess {
-    fn swap_modbus_endianess(&self) -> Self;
+    fn to_swapped_modbus_endianess(&self) -> Self;
 }
 
 impl SwapModbusEndianess for f32 {
-    fn swap_modbus_endianess(&self) -> Self {
+    fn to_swapped_modbus_endianess(&self) -> Self {
         let b = self.to_be_bytes();
         Self::from_be_bytes([b[2], b[3], b[0], b[1]])
     }
 }
 
 impl SwapModbusEndianess for f64 {
-    fn swap_modbus_endianess(&self) -> Self {
+    fn to_swapped_modbus_endianess(&self) -> Self {
         let b = self.to_be_bytes();
         Self::from_be_bytes([b[6], b[7], b[4], b[5], b[2], b[3], b[0], b[1]])
     }
